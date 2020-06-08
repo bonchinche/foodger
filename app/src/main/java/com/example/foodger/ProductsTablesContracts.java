7010b0776b9c46a978d1dfaa2e187390537f581e
DIEMVS <sataev.dima@mail.ru>
2020-06-08 22:54:35 +0300

    Рабочая версия

--- app/src/main/java/com/example/foodger/ProductsTablesContracts.java
+++ app/src/main/java/com/example/foodger/ProductsTablesContracts.java
@@ -14,7 +14,6 @@ public class ProductsTablesContracts {
 
         public final static String _ID= BaseColumns._ID;
         public final static String PRODUCT_TYPE_ID="PRODUCT_TYPE_ID";
-        public final static String PRODUCT_CHARACTERISTIC_ID = "PRODUCT_CHARACTERISTIC_ID";
         public final static String DOM = "DOM";
         public final static String DOS = "DOS";
         public final static String SHELF_LIFE = "SHELF_LIFE";
