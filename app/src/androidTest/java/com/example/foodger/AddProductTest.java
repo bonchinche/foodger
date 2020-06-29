05c03ec01a0be0af003fc3b8742fe98cb467c87e
romik2512 <mazaevroman@mail.ru>
2020-06-29 11:58:34 +0300

    Update AddProductTest.java

--- app/src/androidTest/java/com/example/foodger/AddProductTest.java
+++ app/src/androidTest/java/com/example/foodger/AddProductTest.java
@@ -49,9 +49,11 @@ public class AddProductTest {
     }
 
     public void fillText(int id, String text) {
-       // onView(withId(id)).perform(scrollTo(),click());
-        onView(withId(id)).perform(click());
+        Espresso.closeSoftKeyboard();
+        onView(withId(id)).perform(scrollTo(),click());
+        //onView(withId(id)).perform(click());
         onView(withId(id)).perform(typeText(text));
+        Espresso.closeSoftKeyboard();
     }
 
 
@@ -70,6 +72,7 @@ public class AddProductTest {
     public void testFull() {
         openNavScreen("Add Product");
         onView(withId(R.id.productNameEditText)).perform(typeText("MilkFull"));
+        Espresso.closeSoftKeyboard();
         onView(withId(R.id.additionalnfoButton)).perform(click());
         fillText(R.id.temperatureEditText, "12");
         fillText(R.id.caloriesTextEdit, "30");
@@ -79,26 +82,41 @@ public class AddProductTest {
         fillText(R.id.shelfLifeTextView, "7");
         Espresso.closeSoftKeyboard();
         onView(withId(R.id.productRatingBar)).perform(RatingBarValue.setValue(4));
+        Espresso.closeSoftKeyboard();
         onView(withText("ОК")).perform(click());
+        Espresso.closeSoftKeyboard();
         onView(withId(R.id.applyButton)).perform(click());
+        Espresso.closeSoftKeyboard();
         openNavScreen("Products");
+        Espresso.closeSoftKeyboard();
         onView(withText("MilkFull")).perform(click());
+        Espresso.closeSoftKeyboard();
         onView(withId(R.id.editTemperature)).check(matches(withText("12")));
+        Espresso.closeSoftKeyboard();
         onView(withId(R.id.editcalories)).check(matches(withText("30")));
+        Espresso.closeSoftKeyboard();
         onView(withId(R.id.editprotein)).check(matches(withText("40")));
+        Espresso.closeSoftKeyboard();
         onView(withId(R.id.editcarbohydrates)).check(matches(withText("40")));
+        Espresso.closeSoftKeyboard();
         onView(withId(R.id.editfatness)).check(matches(withText("50")));
+        Espresso.closeSoftKeyboard();
         onView(withId(R.id.editShelf)).check(matches(withText("7")));
+        Espresso.closeSoftKeyboard();
         onView(withId(R.id.Rating)).check(matches(RatingBarValue.checkValue(4)));
+        Espresso.closeSoftKeyboard();
         Espresso.pressBack();
         onView(withId(R.id.deleteButton)).perform(click());
-        onView(withText("ДА")).perform(click());
+        Espresso.closeSoftKeyboard();
+        onView(withId(android.R.id.button1)).perform(click());
+        Espresso.closeSoftKeyboard();
     }
 
     @Test //Добавление продукта температуры хранения
     public void testWithoutTemperature() {
         openNavScreen("Add Product");
         onView(withId(R.id.productNameEditText)).perform(typeText("MilkWithoutTemperature"));
+        Espresso.closeSoftKeyboard();
         onView(withId(R.id.additionalnfoButton)).perform(click());
         fillText(R.id.caloriesTextEdit, "30");
         fillText(R.id.proteinTextEdit, "40");
@@ -107,17 +125,24 @@ public class AddProductTest {
         fillText(R.id.shelfLifeTextView, "7");
         Espresso.closeSoftKeyboard();
         onView(withId(R.id.productRatingBar)).perform(RatingBarValue.setValue(4));
-        onView(withText("ОК")).perform(click());
+        Espresso.closeSoftKeyboard();
+        onView(withId(android.R.id.button1)).perform(click());
+        Espresso.closeSoftKeyboard();
         onView(withId(R.id.applyButton)).perform(click());
+        Espresso.closeSoftKeyboard();
         openNavScreen("Products");
+        Espresso.closeSoftKeyboard();
         onView(withId(R.id.deleteButton)).perform(click());
-        onView(withText("ДА")).perform(click());
+        Espresso.closeSoftKeyboard();
+        onView(withId(android.R.id.button1)).perform(click());
+        Espresso.closeSoftKeyboard();
     }
 
     @Test //Добавление продукта без калорий
     public void testWithoutCalories() {
         openNavScreen("Add Product");
         onView(withId(R.id.productNameEditText)).perform(typeText("MilkWithoutCalories"));
+        Espresso.closeSoftKeyboard();
         onView(withId(R.id.additionalnfoButton)).perform(click());
         fillText(R.id.temperatureEditText, "12");
         fillText(R.id.proteinTextEdit, "40");
@@ -126,11 +151,17 @@ public class AddProductTest {
         fillText(R.id.shelfLifeTextView, "7");
         Espresso.closeSoftKeyboard();
         onView(withId(R.id.productRatingBar)).perform(RatingBarValue.setValue(4));
-        onView(withText("ОК")).perform(click());
+        Espresso.closeSoftKeyboard();
+        onView(withId(android.R.id.button1)).perform(click());
+        Espresso.closeSoftKeyboard();
         onView(withId(R.id.applyButton)).perform(click());
+        Espresso.closeSoftKeyboard();
         openNavScreen("Products");
+        Espresso.closeSoftKeyboard();
         onView(withId(R.id.deleteButton)).perform(click());
-        onView(withText("ДА")).perform(click());
+        Espresso.closeSoftKeyboard();
+        onView(withId(android.R.id.button1)).perform(click());
+        Espresso.closeSoftKeyboard();
     }
 
     @Test //Добавление продукта без белков
@@ -153,11 +184,15 @@ public class AddProductTest {
         Espresso.closeSoftKeyboard();
         onView(withId(R.id.productRatingBar)).perform(RatingBarValue.setValue(4));
         Espresso.closeSoftKeyboard();
-        onView(withText("ОК")).perform(click());
+        onView(withId(android.R.id.button1)).perform(click());
+        Espresso.closeSoftKeyboard();
         onView(withId(R.id.applyButton)).perform(click());
+        Espresso.closeSoftKeyboard();
         openNavScreen("Products");
         onView(withId(R.id.deleteButton)).perform(click());
-        onView(withText("ДА")).perform(click());
+        Espresso.closeSoftKeyboard();
+        onView(withId(android.R.id.button1)).perform(click());
+        Espresso.closeSoftKeyboard();
     }
 
     @Test //Добавление продукта без углеводов
@@ -169,22 +204,17 @@ public class AddProductTest {
         Espresso.closeSoftKeyboard();
         fillText(R.id.caloriesTextEdit, "30");
         Espresso.closeSoftKeyboard();
-      //  fillText(R.id.temperatureEditText, "12");
+        fillText(R.id.temperatureEditText, "12");
         Espresso.closeSoftKeyboard();
         fillText(R.id.proteinTextEdit, "40");
         Espresso.closeSoftKeyboard();
-        //fillText(R.id.fatnessTextEdit, "50");
+        fillText(R.id.fatnessTextEdit, "50");
         Espresso.closeSoftKeyboard();
-      //  fillText(R.id.shelfLifeTextView, "7");
+        fillText(R.id.shelfLifeTextView, "7");
         Espresso.closeSoftKeyboard();
-      //  onView(withId(R.id.productRatingBar)).perform(RatingBarValue.setValue(4));
-        Espresso.closeSoftKeyboard();
-       // onView(withText("ОК")).perform(click());
-        //ad.getButton(AlertDialog.BUTTON_POSITIVE).performClick();
-        //int titleId = this.getActivity().getResources()
-            //    .getIdentifier( "alertTitle", "id", "android" );
+        onView(withId(R.id.productRatingBar)).perform(RatingBarValue.setValue(4));
+        Espresso.closeSoftKeyboard();       
         //onView(withText("ОК")).perform(click());
-
         onView(withId(android.R.id.button1)).perform(click());
         Espresso.closeSoftKeyboard();
         onView(withId(R.id.applyButton)).perform(click());
@@ -201,6 +231,7 @@ public class AddProductTest {
     public void testWithoutFatness() {
         openNavScreen("Add Product");
         onView(withId(R.id.productNameEditText)).perform(typeText("MilkWithoutFatness"));
+        Espresso.closeSoftKeyboard();
         onView(withId(R.id.additionalnfoButton)).perform(click());
         fillText(R.id.temperatureEditText, "12");
         fillText(R.id.caloriesTextEdit, "30");
@@ -209,17 +240,23 @@ public class AddProductTest {
         fillText(R.id.shelfLifeTextView, "7");
         Espresso.closeSoftKeyboard();
         onView(withId(R.id.productRatingBar)).perform(RatingBarValue.setValue(4));
-        onView(withText("ОК")).perform(click());
+        Espresso.closeSoftKeyboard();
+        onView(withId(android.R.id.button1)).perform(click());
+        Espresso.closeSoftKeyboard();
         onView(withId(R.id.applyButton)).perform(click());
+        Espresso.closeSoftKeyboard();
         openNavScreen("Products");
         onView(withId(R.id.deleteButton)).perform(click());
-        onView(withText("ДА")).perform(click());
+        Espresso.closeSoftKeyboard();
+        onView(withId(android.R.id.button1)).perform(click());
+        Espresso.closeSoftKeyboard();
     }
 
     @Test //Добавление продукта без срока годности
     public void testWithoutShelfLife() {
         openNavScreen("Add Product");
         onView(withId(R.id.productNameEditText)).perform(typeText("MilkWithoutShelfLife"));
+        Espresso.closeSoftKeyboard();
         onView(withId(R.id.additionalnfoButton)).perform(click());
         fillText(R.id.temperatureEditText, "12");
         fillText(R.id.caloriesTextEdit, "30");
@@ -228,17 +265,24 @@ public class AddProductTest {
         fillText(R.id.fatnessTextEdit, "50");
         Espresso.closeSoftKeyboard();
         onView(withId(R.id.productRatingBar)).perform(RatingBarValue.setValue(4));
-        onView(withText("ОК")).perform(click());
+        Espresso.closeSoftKeyboard();
+        onView(withId(android.R.id.button1)).perform(click());
+        Espresso.closeSoftKeyboard();
         onView(withId(R.id.applyButton)).perform(click());
+        Espresso.closeSoftKeyboard();
         openNavScreen("Products");
+        Espresso.closeSoftKeyboard();
         onView(withId(R.id.deleteButton)).perform(click());
-        onView(withText("ДА")).perform(click());
+        Espresso.closeSoftKeyboard();
+        onView(withId(android.R.id.button1)).perform(click());
+        Espresso.closeSoftKeyboard();
     }
 
     @Test //Добавление продукта без рейтинга
     public void testWithoutRating() {
         openNavScreen("Add Product");
         onView(withId(R.id.productNameEditText)).perform(typeText("MilkWithoutRating"));
+        Espresso.closeSoftKeyboard();
         onView(withId(R.id.additionalnfoButton)).perform(click());
         fillText(R.id.temperatureEditText, "12");
         fillText(R.id.caloriesTextEdit, "30");
@@ -247,11 +291,16 @@ public class AddProductTest {
         fillText(R.id.fatnessTextEdit, "50");
         fillText(R.id.shelfLifeTextView, "7");
         Espresso.closeSoftKeyboard();
-        onView(withText("ОК")).perform(click());
+        onView(withId(android.R.id.button1)).perform(click());
+        Espresso.closeSoftKeyboard();
         onView(withId(R.id.applyButton)).perform(click());
+        Espresso.closeSoftKeyboard();
         openNavScreen("Products");
+        Espresso.closeSoftKeyboard();
         onView(withId(R.id.deleteButton)).perform(click());
-        onView(withText("ДА")).perform(click());
+        Espresso.closeSoftKeyboard();
+        onView(withId(android.R.id.button1)).perform(click());
+        Espresso.closeSoftKeyboard();
     }
 
     // TODO: 11.06.2020 Добавление продукта с большими значениями
@@ -260,6 +309,7 @@ public class AddProductTest {
     public void testAdditionalInfoCancel() {
         openNavScreen("Add Product");
         onView(withId(R.id.productNameEditText)).perform(typeText("MilkAdditionalInfoCancel"));
+        Espresso.closeSoftKeyboard();
         onView(withId(R.id.additionalnfoButton)).perform(click());
         fillText(R.id.temperatureEditText, "12");
         fillText(R.id.caloriesTextEdit, "30");
@@ -269,21 +319,32 @@ public class AddProductTest {
         fillText(R.id.shelfLifeTextView, "7");
         Espresso.closeSoftKeyboard();
         onView(withId(R.id.productRatingBar)).perform(RatingBarValue.setValue(4));
-        onView(withText("ОТМЕНА")).perform(click());
+        Espresso.closeSoftKeyboard();
+        onView(withId(android.R.id.button2)).perform(click());
+        Espresso.closeSoftKeyboard();
         onView(withId(R.id.additionalnfoButton)).perform(click());
+        Espresso.closeSoftKeyboard();
         onView(withId(R.id.temperatureEditText)).check(matches(withText("")));
+        Espresso.closeSoftKeyboard();
         onView(withId(R.id.caloriesTextEdit)).check(matches(withText("")));
+        Espresso.closeSoftKeyboard();
         onView(withId(R.id.proteinTextEdit)).check(matches(withText("")));
+        Espresso.closeSoftKeyboard();
         onView(withId(R.id.carbohydratesTextEdit)).check(matches(withText("")));
+        Espresso.closeSoftKeyboard();
         onView(withId(R.id.fatnessTextEdit)).check(matches(withText("")));
+        Espresso.closeSoftKeyboard();
         onView(withId(R.id.shelfLifeTextView)).check(matches(withText("")));
+        Espresso.closeSoftKeyboard();
         onView(withId(R.id.productRatingBar)).check(matches(RatingBarValue.checkValue(0)));
+        Espresso.closeSoftKeyboard();
     }
 
     @Test //Тест кнопки отмена в Add Products
     public void testAddProductsCancel() {
         openNavScreen("Add Product");
         onView(withId(R.id.productNameEditText)).perform(typeText("MilkAddProductsCancel"));
+        Espresso.closeSoftKeyboard();
         onView(withId(R.id.additionalnfoButton)).perform(click());
         fillText(R.id.temperatureEditText, "12");
         fillText(R.id.caloriesTextEdit, "30");
@@ -293,17 +354,29 @@ public class AddProductTest {
         fillText(R.id.shelfLifeTextView, "7");
         Espresso.closeSoftKeyboard();
         onView(withId(R.id.productRatingBar)).perform(RatingBarValue.setValue(4));
-        onView(withText("ОК")).perform(click());
+        Espresso.closeSoftKeyboard();
+        onView(withId(android.R.id.button1)).perform(click());
+        Espresso.closeSoftKeyboard();
         onView(withId(R.id.cancellButton)).perform(click());
+        Espresso.closeSoftKeyboard();
         onView(withId(R.id.productNameEditText)).check(matches(withText("")));
+        Espresso.closeSoftKeyboard();
         onView(withId(R.id.additionalnfoButton)).perform(click());
+        Espresso.closeSoftKeyboard();
         onView(withId(R.id.temperatureEditText)).check(matches(withText("")));
+        Espresso.closeSoftKeyboard();
         onView(withId(R.id.caloriesTextEdit)).check(matches(withText("")));
+        Espresso.closeSoftKeyboard();
         onView(withId(R.id.proteinTextEdit)).check(matches(withText("")));
+        Espresso.closeSoftKeyboard();
         onView(withId(R.id.carbohydratesTextEdit)).check(matches(withText("")));
+        Espresso.closeSoftKeyboard();
         onView(withId(R.id.fatnessTextEdit)).check(matches(withText("")));
+        Espresso.closeSoftKeyboard();
         onView(withId(R.id.shelfLifeTextView)).check(matches(withText("")));
+        Espresso.closeSoftKeyboard();
         onView(withId(R.id.productRatingBar)).check(matches(RatingBarValue.checkValue(0)));
+        Espresso.closeSoftKeyboard();
     }
 
     @Test //Тест на занесение в базу данных при нажатии Apply Button
@@ -312,6 +385,7 @@ public class AddProductTest {
         _db = new DataBaseHelper(InstrumentationRegistry.getInstrumentation().getTargetContext());
         openNavScreen("Add Product");
         onView(withId(R.id.productNameEditText)).perform(typeText("MilkSetInDataBase"));
+        Espresso.closeSoftKeyboard();
         onView(withId(R.id.additionalnfoButton)).perform(click());
         fillText(R.id.temperatureEditText, "12");
         fillText(R.id.caloriesTextEdit, "30");
@@ -321,8 +395,11 @@ public class AddProductTest {
         fillText(R.id.shelfLifeTextView, "7");
         Espresso.closeSoftKeyboard();
         onView(withId(R.id.productRatingBar)).perform(RatingBarValue.setValue(4));
-        onView(withText("ОК")).perform(click());
+        Espresso.closeSoftKeyboard();
+       onView(withId(android.R.id.button1)).perform(click());
+        Espresso.closeSoftKeyboard();
         onView(withId(R.id.applyButton)).perform(click());
+        Espresso.closeSoftKeyboard();
         SQLiteDatabase db = _db.getReadableDatabase();
 
         Cursor productsCursor = db.rawQuery("Select * from Products WHERE _id = " + 0, null);
@@ -344,7 +421,10 @@ public class AddProductTest {
         assertEquals("4", productCharacteristicCursor.getString(productCharacteristicCursor.getColumnIndex(ProductsTablesContracts.Product_Characteristic.RATING)));
 
         openNavScreen("Products");
+        Espresso.closeSoftKeyboard();
         onView(withId(R.id.deleteButton)).perform(click());
-        onView(withText("ДА")).perform(click());
+        Espresso.closeSoftKeyboard();
+        onView(withId(android.R.id.button1)).perform(click());
+        Espresso.closeSoftKeyboard();
     }
 }
