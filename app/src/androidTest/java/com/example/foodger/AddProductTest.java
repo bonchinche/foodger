8d48fbc52df740b3d85ba37a4a1395f5e9b5bf48
romik2512 <mazaevroman@mail.ru>
2020-06-29 14:14:24 +0300

    Update AddProductTest.java

--- app/src/androidTest/java/com/example/foodger/AddProductTest.java
+++ app/src/androidTest/java/com/example/foodger/AddProductTest.java
@@ -51,7 +51,8 @@ public class AddProductTest {
     public void fillText(int id, String text) {
         Espresso.closeSoftKeyboard();
        // onView(withId(R.id.ScrollForEspresso)).perform(scrollTo());
-        onView(withId(R.id.onTopOfScrollView)).perform(scrollTo(), click());
+       // onView(withId(R.id.onTopOfScrollView)).perform(scrollTo(), click());     
+        onView(withId(R.id.ScrollForEspresso)).perform(swipeDown());
         onView(withId(id)).perform(scrollTo(),click());
         //onView(withId(id)).perform(click());
         onView(withId(id)).perform(typeText(text));
